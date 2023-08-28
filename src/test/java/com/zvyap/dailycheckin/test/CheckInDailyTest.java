2ea5d3792c92832654b30847eeea7324dd5084ac
Pham Khanh Nhan <78801337+kleqing@users.noreply.github.com>
2023-08-28 10:51:36 +0700

    Add Hi3rd

--- src/test/java/com/zvyap/dailycheckin/test/CheckInDailyTest.java
+++ src/test/java/com/zvyap/dailycheckin/test/CheckInDailyTest.java
@@ -26,6 +26,7 @@ public class CheckInDailyTest {
                         .token(HoyoToken.of(System.getenv("USER_1_ID"), System.getenv("USER_1_TOKEN")))
                         .game(GameType.GENSHIN_IMPACT)
                         .game(GameType.HONKAI_STAR_RAIL)
+                        .game(GameType.HONKAI_IMPACT_3RD)
                         .build()
 
 
